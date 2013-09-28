d39b82bb30e0736b1df9faf84902398cfee5faa0
Gruby <pawel.kozlowski@hotmail.com>
2013-09-28 23:37:14 +0200

    Refactoring: player is global. Bubbles for story telling

--- StateExplore.lua
+++ StateExplore.lua
@@ -1,7 +1,9 @@
+Camera = require "Lib.hump.camera"
 require "IfiWorld"
 require "Monster"
 
 local ifiWorld
+local camera = Camera()
 local fogImg
 
 StateExplore = {}
@@ -20,13 +22,24 @@ end
 
 function StateExplore:update(dt)
   ifiWorld:update(dt)
+  player:update(dt)
+  -- update camera
+  local playerActPixelX, playerActPixelY = player:getActPixelPos()
+  -- camera should be focused on the middle of the player character
+  playerActPixelX = playerActPixelX + TILE_SIZE / 2
+  playerActPixelY = playerActPixelY + TILE_SIZE / 2
+  camera:lookAt(playerActPixelX, playerActPixelY)
+
   for n,b in pairs(state.button) do
     b:update(dt)
   end
 end
 
 function StateExplore:draw()
-	ifiWorld:draw()
+  camera:attach()
+	ifiWorld:draw(camera:pos())
+  player:draw()
+  camera:detach()
   --love.graphics.draw(fogImg, 0, 0)
   for n,b in pairs(state.button) do
     if  n == "testButton" then b:draw() end
@@ -55,25 +68,49 @@ function StateExplore
       end
     end
   end
-  
 end
-
-
 
 function StateExplore:keyreleased(key, unicode)
 	if key == "up" then
-    	ifiWorld:movePlayer(0, -1)
+    	movePlayer(0, -1)
   	end
   	if key == "down" then
-    	ifiWorld:movePlayer(0, 1)
+    	movePlayer(0, 1)
   	end
   	if key == "left" then
-    	ifiWorld:movePlayer(-1, 0)
+    	movePlayer(-1, 0)
   	end
   	if key == "right" then
-    	ifiWorld:movePlayer(1, 0)
+    	movePlayer(1, 0)
   	end
   	if key == "b" then
   		Gamestate.switch(StateBattle)
   	end
+end
+
+function movePlayer(dx, dy)
+  local newPlayerX = player:getX() + dx
+  local newPlayerY = player:getY() + dy
+  if ifiWorld:tileIsFree(newPlayerX, newPlayerY) then
+    player:setPos(newPlayerX, newPlayerY)
+    -- check for battle
+    local monstersLayerId = ifiWorld:getLayerId("monsters")
+    local monsterId = ifiWorld:getTileId(newPlayerX, newPlayerY, monstersLayerId)
+    if monsterId ~= 0 then
+      Gamestate.switch(StateBattle)
+    end
+    -- check for nut
+    local pickableLayerId = ifiWorld:getLayerId("pickable")
+    local pickableId = ifiWorld:getTileId(newPlayerX, newPlayerY, pickableLayerId)
+    if pickableId == NUT_TILE then
+      ifiWorld:setTileId(newPlayerX, newPlayerY, pickableLayerId, EMPTY_TILE)
+      local newCollectedNuts = player:getCollectedNuts() + 1
+      player:setCollectedNuts(newCollectedNuts)
+    end
+  else
+    local templateLayerId = ifiWorld:getLayerId("template")
+    if ifiWorld:getTileId(newPlayerX, newPlayerY, templateLayerId) == METAL_DOOR then
+      ifiWorld:setDoorBubbleTimer(3)
+    end
+  end
 end
