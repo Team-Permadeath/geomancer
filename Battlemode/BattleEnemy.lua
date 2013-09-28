1e41b6d891d51ee313ff2bb89e6b8f8ac63cf9c5
veronahe <veronika.heim@gmail.com>
2013-09-28 14:02:39 +0200

    Refactored stateBattle

---
+++ Battlemode/BattleEnemy.lua
@@ -0,0 +1,15 @@
+Class = require "Lib.hump.class"
+
+BattleEnemy = Class{
+	init = function(self, enemy, battleMap, x, y)
+		self.enemy = enemy
+		self.battleMap = battleMap
+		self.x = x * TILE_SIZE
+		self.y = y * TILE_SIZE
+	end
+}
+
+function BattleEnemy:draw()
+	self.x = self.x + 1
+	self.y = self.y + 1
+end
