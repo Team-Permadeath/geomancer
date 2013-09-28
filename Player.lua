982a446feeed0c5bf64e02db06ae7f64de5640fd
Gruby <pawel.kozlowski@hotmail.com>
2013-09-28 14:23:01 +0200

    character animation

--- Player.lua
+++ Player.lua
@@ -1,18 +1,18 @@
 Class = require "Lib.hump.class"
+Vector = require "Lib.hump.vector-light"
+require "Lib.AnAL"
 
 Player = Class{
-  init = function(self, x, y, tileSize, sprite, speed)
+  init = function(self, x, y, tileSize, animSprite, speed)
     self.x = x
     self.y = y
     self.tileSize = tileSize
-    self.sprite = sprite
+    self.animSprite = animSprite
     self.speed = speed
     self.gridPixelX = x * tileSize
     self.gridPixelY = y * tileSize
     self.actPixelX = x * tileSize
     self.actPixelY = y * tileSize
-    self.tmpAnimSprite1 = love.graphics.newImage("Images/river.png")
-    self.tmpAnimSprite2 = sprite
   end,
   getX = function(self)
     return self.x
@@ -41,18 +41,13 @@ Player = Class{
   update = function(self, dt)
     self.actPixelX = self.actPixelX - ((self.actPixelX - self.gridPixelX) * self.speed * dt)
     self.actPixelY = self.actPixelY - ((self.actPixelY - self.gridPixelY) * self.speed * dt)
-    dist = self:tmpDistToGo()
-    if (0.1 * self.tileSize < dist and dist < 0.25 * self.tileSize) or (0.5 * self.tileSize < dist and dist < 0.75 * self.tileSize) then
-      self.sprite = self.tmpAnimSprite1
-    else
-      self.sprite = self.tmpAnimSprite2
+    -- some threshold for animation
+    if 0.05 * self.tileSize < Vector.dist(self.actPixelX, self.actPixelY, self.gridPixelX, self.gridPixelY) then
+      self.animSprite:update(dt)
     end
   end,
   draw = function(self)
-    love.graphics.draw(self.sprite, self.actPixelX, self.actPixelY)
+    --love.graphics.draw(self.sprite, self.actPixelX, self.actPixelY)
+    self.animSprite:draw(self.actPixelX, self.actPixelY)
   end,
-
-  tmpDistToGo = function(self)
-    return math.sqrt(math.pow(self.actPixelX - self.gridPixelX, 2) + math.pow(self.actPixelY - self.gridPixelY, 2))
-  end
 }
