d9b517fb057f5c9d4e0d56c9449642b0c2642c13
veronahe <veronika.heim@gmail.com>
2013-09-29 11:26:29 +0200

    Changed image for health

--- Battlemode/BattleHealth.lua
+++ Battlemode/BattleHealth.lua
@@ -4,12 +4,12 @@ BattleHealth = Class {
 	init = function (self, x, y)
 		self.x = x
 		self.y = y
-		self.image = love.graphics.newImage("Images/cards-07.png")
+		self.image = love.graphics.newImage("Images/health-01.png")
 	end,
 	draw = function (self, maxHealth)
 		love.graphics.setColor(255, 255, 255)
 		for i=1, maxHealth do
-			love.graphics.draw(self.image, self.x + i * 30, self.y, 0, 0.3)
+			love.graphics.draw(self.image, self.x + i * 30, self.y)
 		end
 	end
 }
