07040ec316def7a06a103b662474fb1a4a58748f
TZer0 <tzer0.jan@gmail.com>
2013-09-29 16:40:20 +0200

    Minor fix.

--- Particles.lua
+++ Particles.lua
@@ -5,9 +5,8 @@ ParticleSystems = {}
 local Particle = love.graphics.newParticleSystem(love.graphics.newImage("Images/fireparticle.png"), 1000);
 Particle:setEmissionRate(1000)
 Particle:setSpeed(200, 300)
-Particle:setSizes(2, 1)
+Particle:setSizes(1, 1)
 --Particle:setColors(220, 105, 20, 255, 194, 30, 18, 0)
-Particle:setPosition(400, 300)
 Particle:setLifetime(0.1)
 Particle:setParticleLife(0.2)
 Particle:setDirection(0)
@@ -15,16 +14,14 @@ Particle:setSpread(360)
 Particle:setTangentialAcceleration(1000)
 Particle:setRadialAcceleration(-2000)
 
-Particle:setPosition(50, 50)
 Particle:stop()
 ParticleSystems["Fire"] = Particle
 
 local Particle2 = love.graphics.newParticleSystem(love.graphics.newImage("Images/waterparticle.png"), 1000);
 Particle2:setEmissionRate(1000)
 Particle2:setSpeed(300, 400)
-Particle2:setSizes(2, 1)
+Particle2:setSizes(1, 1)
 --Particle2:setColors(220, 105, 20, 255, 194, 30, 18, 0)
-Particle2:setPosition(400, 300)
 Particle2:setLifetime(0.1)
 Particle2:setParticleLife(0.2)
 Particle2:setDirection(0)
@@ -32,24 +29,21 @@ Particle2:setSpread(360)
 Particle2:setTangentialAcceleration(1000)
 Particle2:setRadialAcceleration(0)
 
-Particle2:setPosition(50, 50)
 Particle2:stop()
 ParticleSystems["Water"] = Particle2
 
 local Particle3 = love.graphics.newParticleSystem(love.graphics.newImage("Images/sandparticle.png"), 1000);
 Particle3:setEmissionRate(1000)
 Particle3:setSpeed(300, 400)
-Particle3:setSizes(2, 1)
+Particle3:setSizes(1, 1)
 --Particle3:setColors(220, 105, 20, 255, 194, 30, 18, 0)
-Particle3:setPosition(400, 300)
 Particle3:setLifetime(0.1)
 Particle3:setParticleLife(0.2)
 Particle3:setDirection(0)
 Particle3:setSpread(1)
-Particle3:setTangentialAcceleration(1000)
+--Particle3:setTangentialAcceleration(1000)
 Particle3:setRadialAcceleration(0)
 
-Particle3:setPosition(50, 50)
 Particle3:stop()
 ParticleSystems["Sand"] = Particle3
 
